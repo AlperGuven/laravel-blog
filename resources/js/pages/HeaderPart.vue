48ff71f8680f4ae1021ea1492c9fc9ba863dcb3f
Alper Guven <alper.guven@tarfin.com>
2023-12-10 22:50:11 +0300

    Pages and components are added

---
+++ resources/js/pages/HeaderPart.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="container">
+        <hgroup>
+            <h1>Alper's basic blog website</h1>
+            <h2 class="font-21">It is the Blog or whatever</h2>
+        </hgroup>
+    </header>
+</template>
+
+<script setup></script>
+<style scoped></style>

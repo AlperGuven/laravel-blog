eeb50dc6e5f3859739c6874473b4dc111f5a1c7a
Alper Guven <alper.guven@tarfin.com>
2023-12-10 22:49:03 +0300

    DB columns and factory edits

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,6 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
